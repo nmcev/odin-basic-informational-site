704bb523cda685550e27d4c7bc170a76bcbf258f
muha <1student.muhammed1@gmail.com>
2024-03-19 17:42:55 +0300

    refactor: replace vanilla javaScript with express

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 const path = require('path')
 const port = process.env.PORT || 8080;
 
 
-http.createServer((req, res) => {
-    const userReq = req.url;
-    let pathToFile;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"));
+})
+
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, "contact-me.html"));
+})
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
-    if (req.url.endsWith(".css")) {
-        pathToFile = path.join(__dirname, userReq);
-    } else {
-        switch (userReq) {
-            case "/":
-                pathToFile = path.join(__dirname, "index.html");
-                break;
-            case "/about":
-                pathToFile = path.join(__dirname, "about.html")
-                break;
-            case "/contact-me":
-                pathToFile = path.join(__dirname, "contact-me.html")
-                break;
-            default:
-                pathToFile = path.join(__dirname, "404.html")
-                break;
-        }
-    }
-
-    fs.readFile(pathToFile, "utf8", function (err, content) {
-        if (err) {
-            res.writeHead(404, { "Content-Type": "text/plain" })
-            res.end("404")
-        } else {
-            const contentType = getContentType(pathToFile)
-            res.writeHead(200, { "Content-Type": contentType })
-            res.end(content)
-        }
-
-    })
-}).listen(port, () => console.log("Server running on port: " + port))
-
-
-function getContentType(filePath) {
-    const extname = path.extname(filePath)
-
-    switch (extname) {
-        case ".html":
-            return "text/html";
-        case ".css":
-            return "text/css";
-        default:
-            return "text/plain";
-    }
-}
